51765f8f715764a132a8504bfcb3164fbac77391
harrisonlim <harrison.lim.hx@gmail.com>
2015-11-27 09:01:33 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nomnivore::Application.config.secret_key_base = '6bcbd624510a3a4b01b600c8f76b055589b09f5197498a7a499ce4a3fdaf7a6e60f5d12fd743e6788db0075587ed71029819672ab55195cca29aaba142e2b9cb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Nomnivore::Application.config.session_store :cookie_store, key: '_nomnivore_session'

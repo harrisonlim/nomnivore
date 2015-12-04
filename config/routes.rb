34e66fe9e46bf2b62577906b75ac36a8d8c2f0bc
harrisonlim <harrison.lim.hx@gmail.com>
2015-12-04 08:43:38 +0000

    added pagination class, add resource :places to routes file

--- config/routes.rb
+++ config/routes.rb
@@ -56,4 +56,8 @@ Nomnivore::Application.routes.draw do
 
 root 'places#index'
 
+resources :places
+# The priority is based upon order of creation: first created -> highest priority.
+# More comments here ......
+
 end

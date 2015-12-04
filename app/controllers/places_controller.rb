9270347b94699e263103061429ff59dff4284e38
harrisonlim <harrison.lim.hx@gmail.com>
2015-12-04 09:32:32 +0000

    modified places_controller to draw user submitted information and redirect users to root_path where it is displayed

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -8,4 +8,14 @@ class PlacesController < ApplicationController
 		@places = Place.new
 	end
 
+	def create
+		Place.create(place_params)
+		redirect_to root_path
+	end
+
+	def place_params
+		#placed :address before :description, try switching if it throws out an error
+		params.require(:place).permit(:name, :address, :description)
+	end
+
 end

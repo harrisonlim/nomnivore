31832328481f296b9ffe1de96e7f3c880e1ff9ba
harrisonlim <harrison.lim.hx@gmail.com>
2016-01-19 17:14:31 +0000

    added comments controller test

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create comment" do
+  	user = FactoryGirl.create(:user)
+    sign_in user
+
+	  assert_difference 'Comment.count' do
+	  	post :create, {:place_id => place.id, :comment => {
+	  		:message => 'yolo',
+	  		:rating => '1_star',
+	        }
+	      }
+  	  end
+  end
+
+  assert_equal 1, user.comments.count
+  assert_redirected_to places_path
 end
--- test/models/place_test.rb
+++ test/models/place_test.rb
@@ -1,24 +1,24 @@
-require 'test_helper'
+# require 'test_helper'
 
-class PlaceTest < ActiveSupport::TestCase
+# class PlaceTest < ActiveSupport::TestCase
 
-test "create" do 
-    user = FactoryGirl.create(:user)
-    sign_in user
+# test "create" do 
+#   #   user = FactoryGirl.create(:user)
+#   #   sign_in user
 
-    assert_difference 'Place.count' do
-    post :create, {:place => {
-    	:name => 'yolo',
-    	:description => 'omg',
-    	:address => '7 Sunrise Lane',
-    	:latitude => 42.3631519,
-        :longitude => -71.056098,
-    	}
-    }
-  	end
+#   #   assert_difference 'Place.count' do
+#   #   post :create, {:place => {
+#   #   	:name => 'yolo',
+#   #   	:description => 'omg',
+#   #   	:address => '7 Sunrise Lane',
+#   #   	:latitude => 42.3631519,
+#   #       :longitude => -71.056098,
+#   #   	}
+#   #   }
+#   # 	end
 
-  	assert_redirected_to places_path
+#   # 	assert_redirected_to places_path
 
-  	assert_equal 1, user.places.count
-  end
-end
+#   # 	assert_equal 1, user.places.count
+#   # end
+# end
